7a6391b4a4aa9a8423ecad92d7bb72783b0a9b22
Rohit Bhatta <rbhatta@cs.utexas.edu>
2016-03-05 15:34:50 -0600

    Passed all basic tests

--- src/thunk.c
+++ src/thunk.c
@@ -30,12 +30,20 @@ static uint8_t* xalloc() {
     
 
 F0 thunk1(F1 f, long a0) {
-     uint8_t* p = xalloc();
-     //MISSING(15);
-     //move something to rdi
-     //mov a0 to rdi
-     //move something to rax
-     //mov f to rax
-     //jump to rax
-     return (F0)p;
+    uint8_t* p = xalloc();
+    //MISSING(15);
+    //mov a0 to rdi
+    //mov f to rax
+    //jump to rax
+    p[0] = 0x48;
+    p[1] = 0xbf;
+    //line here
+    *(long*) &(p[2]) = a0;
+    p[0xa] = 0x48;
+    p[0xb] = 0xb8;
+    //line here
+    *(long*) &(p[0xc]) = (long) f;
+    p[0x14] = 0xff;
+    p[0x15] = 0xe0;
+    return (F0)p;
 }
